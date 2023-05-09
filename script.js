9c3652fb43dc841cf93065d1f7f30e0761c01a59
tomer <tomer@gmail.com>
2023-05-09 03:39:21 -0700

    script push

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const curr = document.getElementBy
+class Calculator {
+    constructor(){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.CurrentOperandTextElement = CurrentOperandTextElement
+    }
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(){
+        
+    }
+}
+
+const numButton = document.querySelectorAll('[data-number]')
+const operationButton = document.querySelectorAll('[data-operand]')
+const  dataAllClear = document.querySelector('[data-allclear]')
+const  equalButton = document.querySelector('[data-equals]')
+const  deleteButton = document.querySelector('[data-delete]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const CurrentOperandTextElement = document.querySelector('[data-Current-operand]')
+
+console.log(numButton)
